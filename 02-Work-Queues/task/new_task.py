1dd7e58590bf281ab20bd4e93d4b89580f50c0a9
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-18 23:13:03 +0330

    add credentials

--- 02-Work-Queues/task/new_task.py
+++ 02-Work-Queues/task/new_task.py
@@ -6,6 +6,8 @@ username = os.environ['RABBITMQ_DEFAULT_USER']
 password = os.environ['RABBITMQ_DEFAULT_PASS']
 
 try:
+    # Set the connection parameters to connect to localhost on port 5672
+    credentials = pika.PlainCredentials(username=username, password=password)
 
 except pika.exceptions as e:
     print(f'exception is: {e}')
