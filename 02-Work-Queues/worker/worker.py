6456f025258f21bafae5a150fff5726cb5d7b1d0
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-19 20:32:51 +0330

    add try except

--- 02-Work-Queues/worker/worker.py
+++ 02-Work-Queues/worker/worker.py
@@ -4,3 +4,10 @@ import uuid
 
 username = os.environ['RABBITMQ_DEFAULT_USER']
 password = os.environ['RABBITMQ_DEFAULT_PASS']
+
+try:
+    # Set the connection parameters to connect to localhost on port 5672
+    credentials = pika.PlainCredentials(username=username, password=password)
+
+except pika.exceptions as e:
+    print(f'exception is: {e}')
