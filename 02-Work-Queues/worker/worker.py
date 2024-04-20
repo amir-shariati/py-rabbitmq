6736fdf56530c18a2c2b2e85260b7c887373937c
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-20 19:44:21 +0330

    add ConnectionParameters

--- 02-Work-Queues/worker/worker.py
+++ 02-Work-Queues/worker/worker.py
@@ -31,5 +31,8 @@ try:
     # Set the connection parameters to connect to localhost on port 5672
     credentials = pika.PlainCredentials(username=username, password=password)
 
+    parameters = pika.ConnectionParameters(host='rabbitmq_server', port=5672, credentials=credentials)
+
+
 except pika.exceptions as e:
     print(f'exception is: {e}')
