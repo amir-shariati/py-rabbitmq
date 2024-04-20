325a160b069ef622305b43a5fa464d898336b7ce
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-20 19:44:08 +0330

    add on_message_callback_func

--- 02-Work-Queues/worker/worker.py
+++ 02-Work-Queues/worker/worker.py
@@ -14,6 +14,19 @@ class BodyType(TypedDict):
 username = os.environ['RABBITMQ_DEFAULT_USER']
 password = os.environ['RABBITMQ_DEFAULT_PASS']
 
+def on_message_callback_func(
+        channel: pika.adapters.blocking_connection.BlockingChannel,
+        method: pika.spec.Basic.Deliver,
+        properties: pika.spec.BasicProperties,
+        body: bytes
+):
+    # print(f' [x] Received {body.decode()}')
+    body: BodyType = json.loads(body)
+    print(f' [x] Received {body}')
+    time.sleep(body['delay'])
+    channel.basic_ack(delivery_tag=method.delivery_tag)
+    print(f' [x] Task: {body["task_id"]}, Done after {body["delay"]}\'s ')
+
 try:
     # Set the connection parameters to connect to localhost on port 5672
     credentials = pika.PlainCredentials(username=username, password=password)
