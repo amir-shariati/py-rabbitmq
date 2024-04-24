0d3e9ed3ad9050e794af6e24cb2d43e07a2f5591
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-24 23:58:50 +0330

    add message

--- 03-Publish-Subscribe/emit/emit_log.py
+++ 03-Publish-Subscribe/emit/emit_log.py
@@ -12,5 +12,27 @@ password = os.environ['RABBITMQ_DEFAULT_PASS']
 
 
 def publish_msg(ch: pika.adapters.blocking_connection.BlockingChannel):
+    message = {
+        'log_id': uuid.uuid4().hex,
+        'time': datetime.datetime.now().time().strftime("%H:%M:%S"),
+        'log_msg': f'this is log {random.randint(1,10)}'
+    }
 
 
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='logs', exchange_type=ExchangeType.fanout)
+
+    time.sleep(1)
+    publish_msg(ch=channel)
+    time.sleep(2)
+    publish_msg(ch=channel)
+    time.sleep(2)
+    publish_msg(ch=channel)
+
+
+except pika.exceptions as e:
+    print(f'exception is: {e}')
+
+else:
+    connection.close()
