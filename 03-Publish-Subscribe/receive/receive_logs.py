2cce5723396a8903464a117bd97639ba04688739
amirshariati <seyed.amir.shariati@gmail.com>
2024-04-25 00:03:54 +0330

    load body

--- 03-Publish-Subscribe/receive/receive_logs.py
+++ 03-Publish-Subscribe/receive/receive_logs.py
@@ -21,5 +21,13 @@ def on_message_callback_func(
         properties: pika.spec.BasicProperties,
         body: bytes
 ):
+    # print(f' [x] Received {body.decode()}')
+    body: BodyType = json.loads(body)
+    print(f' [x] Received {body}')
+    # time.sleep(body['delay'])
+    # ch.basic_ack(delivery_tag=method.delivery_tag)
+    print(f' [x] LogID: {body["log_id"]}, log message {body["log_msg"]} ')
+
+
 
 
