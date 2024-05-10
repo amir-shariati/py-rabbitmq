58baa7ad23635721968a8565db357b83c06d6628
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-10 19:29:14 +0330

    add BlockingConnection

--- 04-Routing/emit/emit_log_direct.py
+++ 04-Routing/emit/emit_log_direct.py
@@ -24,6 +24,8 @@ try:
     # Set the connection parameters to connect to localhost on port 5672
     credentials = pika.PlainCredentials(username=username, password=password)
     parameters = pika.ConnectionParameters(host='rabbitmq_server', port=5672, credentials=credentials)
+    connection = pika.BlockingConnection(parameters=parameters)
+
 
 except pika.exceptions as e:
     print(f'exception is: {e}')
