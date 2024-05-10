be3bd37e39cf656624d90c017b817d30373b3f47
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-10 19:25:04 +0330

    add publish_msg

--- 04-Routing/emit/emit_log_direct.py
+++ 04-Routing/emit/emit_log_direct.py
@@ -9,3 +9,13 @@ from pika.exchange_type import ExchangeType
 
 username = os.environ['RABBITMQ_DEFAULT_USER']
 password = os.environ['RABBITMQ_DEFAULT_PASS']
+
+
+def publish_msg(ch: pika.adapters.blocking_connection.BlockingChannel):
+    message = {
+        'log_id': uuid.uuid4().hex,
+        'time': datetime.datetime.now().time().strftime("%H:%M:%S"),
+        'log_msg': f'this is log {random.randint(1,10)}'
+    }
+
+    ch.basic_publish(exchange='logs', routing_key='', body=json.dumps(message))
