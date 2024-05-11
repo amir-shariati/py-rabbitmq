64fdc61ade58f54a4f3fcc4e70712e7c8bd68525
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-12 02:16:32 +0330

    add try

--- 04-Routing/receive/receive_logs_direct.py
+++ 04-Routing/receive/receive_logs_direct.py
@@ -31,3 +31,7 @@ def on_message_callback_func(
     print(f' [x] Routing_key {method.routing_key}')
     print(f' [x] LogID: {body["log_id"]}, log message {body["log_msg"]} ')
 
+
+try:
+except pika.exceptions as e:
+    print(f'exception is: {e}')
