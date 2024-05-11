a891ff03e70080fc7e12269c8f315490325d5dad
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-12 02:16:02 +0330

    add on_message_callback_func

--- 04-Routing/receive/receive_logs_direct.py
+++ 04-Routing/receive/receive_logs_direct.py
@@ -20,3 +20,14 @@ log_levels = os.environ['LOG_LEVELS']
 exchange_name = 'direct_logs'
 
 
+def on_message_callback_func(
+        ch: pika.adapters.blocking_connection.BlockingChannel,
+        method: pika.spec.Basic.Deliver,
+        properties: pika.spec.BasicProperties,
+        body: bytes
+):
+    body: BodyType = json.loads(body)
+    print(f' [x] Received {body}')
+    print(f' [x] Routing_key {method.routing_key}')
+    print(f' [x] LogID: {body["log_id"]}, log message {body["log_msg"]} ')
+
