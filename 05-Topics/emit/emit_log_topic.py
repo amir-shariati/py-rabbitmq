eacb7054dd43b6f22cb227996144dd8fb06fdf7f
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-13 19:03:22 +0330

    add publish_msg

--- 05-Topics/emit/emit_log_topic.py
+++ 05-Topics/emit/emit_log_topic.py
@@ -15,3 +15,14 @@ log_topic = os.environ['LOG_TOPIC']
 exchange_name = 'topic_logs'
 
 
+def publish_msg(ch: pika.adapters.blocking_connection.BlockingChannel, log_topic, log_msg):
+    message = {
+        'log_id': uuid.uuid4().hex,
+        'time': datetime.datetime.now().time().strftime("%H:%M:%S"),
+        'log_topic': log_topic,
+        'log_msg': log_msg
+    }
+
+    ch.basic_publish(exchange=exchange_name, routing_key=log_topic, body=json.dumps(message))
+
+
