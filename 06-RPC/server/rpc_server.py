c927f9513edcf255943aba714766ca84fdc64e8e
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-14 17:34:26 +0330

    add connection

--- 06-RPC/server/rpc_server.py
+++ 06-RPC/server/rpc_server.py
@@ -57,6 +57,11 @@ def on_request(
 
 
 try:
+    # Set the connection parameters to connect to localhost on port 5672
+    credentials = pika.PlainCredentials(username=username, password=password)
+    parameters = pika.ConnectionParameters(host='rabbitmq_server', port=5672, credentials=credentials)
+    connection = pika.BlockingConnection(parameters=parameters)
+
 
 except pika.exceptions as e:
     print(f'exception is: {e}')
