d629482bbceb3eafed49de6c44f603e106992725
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-14 17:33:45 +0330

    add on_request

--- 06-RPC/server/rpc_server.py
+++ 06-RPC/server/rpc_server.py
@@ -35,3 +35,24 @@ def publish_msg(ch: pika.adapters.blocking_connection.BlockingChannel, correlati
     ch.basic_ack(delivery_tag=delivery_tag)
 
 
+def on_request(
+        ch: pika.adapters.blocking_connection.BlockingChannel,
+        method: pika.spec.Basic.Deliver,
+        props: pika.spec.BasicProperties,
+        body: bytes
+):
+    body_data: BodyType = json.loads(body)
+    print(f' [x] Received {body_data}')
+    time.sleep(2)
+    res = increment(body_data['request'])
+    body_data['time'] = datetime.datetime.now().time().strftime("%H:%M:%S")
+    body_data['response'] = res
+    publish_msg(
+        ch=ch,
+        correlation_id=props.correlation_id,
+        reply_to=props.reply_to,
+        delivery_tag=method.delivery_tag,
+        data=body_data
+    )
+
+
