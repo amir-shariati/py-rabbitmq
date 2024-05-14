fc5b80e85bd165e56b8c52c90ea11a9c73413476
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-14 17:34:37 +0330

    add channel

--- 06-RPC/server/rpc_server.py
+++ 06-RPC/server/rpc_server.py
@@ -62,6 +62,8 @@ try:
     parameters = pika.ConnectionParameters(host='rabbitmq_server', port=5672, credentials=credentials)
     connection = pika.BlockingConnection(parameters=parameters)
 
+    channel = connection.channel()
+
 
 except pika.exceptions as e:
     print(f'exception is: {e}')
