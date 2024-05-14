96e2e727c66b79ad0fbaf9b23e1f3fd09fcb6ec7
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-14 13:47:49 +0330

    add on_message_callback_func

--- 05-Topics/receive/receive_logs_topic.py
+++ 05-Topics/receive/receive_logs_topic.py
@@ -20,3 +20,15 @@ log_topics = os.environ['LOG_TOPIC']
 exchange_name = 'topic_logs'
 
 
+def on_message_callback_func(
+        ch: pika.adapters.blocking_connection.BlockingChannel,
+        method: pika.spec.Basic.Deliver,
+        properties: pika.spec.BasicProperties,
+        body: bytes
+):
+    body: BodyType = json.loads(body)
+    print(f' [x] Received {body}')
+    print(f' [x] Routing_key {method.routing_key}')
+    print(f' [x] LogID: {body["log_id"]}, log message {body["log_msg"]} ')
+
+
