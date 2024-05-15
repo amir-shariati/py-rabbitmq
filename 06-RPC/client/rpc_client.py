e1e059d54e586b8f55198087f40a8c267650498f
amirshariati <seyed.amir.shariati@gmail.com>
2024-05-15 19:23:08 +0330

    add imports

---
+++ 06-RPC/client/rpc_client.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+import datetime
+import json
+import os
+import time
+import uuid
+import pika
+from pika.exchange_type import ExchangeType
+from typing import Optional, NotRequired, TypedDict
+
+
